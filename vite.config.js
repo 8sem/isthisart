503ad39e840aa6c5ec52024fdeb4a5739b28d6bd
iswnischay <iswnischay1385@gmail.com>
2025-03-10 20:22:56 +0530

    few changes here and there

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Allow external access
+    allowedHosts: [
+      "localhost",
+      ".ngrok-free.app" // Allows any ngrok tunnel
+    ]
+  }
 })
